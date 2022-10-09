b03d65f6a0fe7caf7158170df694a060d0d48b7e
Mikhail Isupov <miknsk@gmail.com>
2022-10-09 21:16:07 +0700

    исправлена задача 2

--- Seminar1/task2.py
+++ Seminar1/task2.py
@@ -1,8 +1,8 @@
 # Проверка истинности утверждения
-x = int(input('Введите X '))
-y = int(input('Введите Y '))
-z = int(input('Введите Z '))
-if not (x or y or z) == ( not x and not y and not z ):
-    print('Утверждение истинно')
-else:
-    print('Утверждение не истинно')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == ( not x and not y and not z ):
+                print(f'Утверждение истинно для x = {x}, y = {y}, z = {z}')
+            else:
+                print(f'Утверждение не истинно для x = {x}, y = {y}, z = {z}')
