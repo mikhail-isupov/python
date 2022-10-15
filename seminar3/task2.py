4d96c6c149f58af9be6bf4c64da958907c6b909a
Mikhail Isupov <111003222+mikhail-isupov@users.noreply.github.com>
2022-10-15 21:33:37 +0700

    Update seminar3/task2.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- seminar3/task2.py
+++ seminar3/task2.py
@@ -9,6 +9,6 @@ num_list = [rnd.randint(0,9) for i in range(list_len)]
 
 print('Сгенерированный список: ', num_list)
 
-num_list_multiplied = [ num_list[i]*num_list[-(i + 1)] for i in range(list_len // 2 + list_len % 2) ]
+num_list_multiplied = [ num_list[i] * num_list[-i - 1] for i in range((list_len + 1) // 2) ]
 
 print('Список из попарно умноженных элементов: ', num_list_multiplied)
