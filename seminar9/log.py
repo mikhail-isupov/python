99235c09b61746a89d5bfaf9fb3f738c9541a6c5
Mikhail Isupov <111003222+mikhail-isupov@users.noreply.github.com>
2022-12-01 12:42:46 +0700

    Update seminar9/log.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- seminar9/log.py
+++ seminar9/log.py
@@ -4,4 +4,4 @@ def log(update: Update, result):
     dt = str(datetime.now())
     output_string = ' '.join([dt, str(update.effective_user), update.message.text, result, '\n'])
     with open('log.txt', mode='a') as file:
-        file.write(output_string)
+        file.write(output_string + '\n')
